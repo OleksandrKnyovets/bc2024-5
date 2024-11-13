0b489aca0d7e09e8815e364343f9abb224aa69e1
OleksandrKnyovets bc2024-5 <sashakn2018@gmail.com>
2024-11-13 17:02:25 +0200

    Налаштування nodemon для автоматичного перезапуску та перевірка параметрів

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ program
     .requiredOption('-h, --host <type>', 'server address')
     .requiredOption('-p, --port <number>', 'server port')
     .requiredOption('-c, --cache <path>', 'cache directory');
-
 program.parse(process.argv);
 
+// Отримання опцій з командного рядка
 const { host, port, cache } = program.opts();
 
+const app = express();      
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-console.log(`Параметри серверу: хост =${host}, порт =${port}, кеш =${cache}`);
+// Функція для отримання шляху до нотатки
+function getNotePath(name) {
+    return path.join(cache, `${name}.txt`);
+}
+
+// Запуск сервера
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
